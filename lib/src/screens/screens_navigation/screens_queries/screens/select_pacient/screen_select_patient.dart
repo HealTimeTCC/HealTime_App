09b1e261009f5b5d4244a9a3e5a907ca786b3939
Thiago2807 <thiagodasilva410@gmail.com>
2023-04-30 18:54:20 -0300

    Adicionando a parte de selecionar pacientes

---
+++ lib/src/screens/screens_navigation/screens_queries/screens/select_pacient/screen_select_patient.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelectPacient extends StatelessWidget {
+  const SelectPacient({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
