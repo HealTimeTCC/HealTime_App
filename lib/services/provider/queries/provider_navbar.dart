718a671e2c0c25ac03975e5eca273a2e63cba246
Thiago2807 <thiagodasilva410@gmail.com>
2023-05-13 21:21:04 -0300

    Consetando a SplashScreen

--- lib/services/provider/queries/provider_navbar.dart
+++ lib/services/provider/queries/provider_navbar.dart
@@ -1,17 +1,24 @@
-
 import 'package:flutter/material.dart';
 
 import '../../../src/screens/screens_navigation/home_page/home.dart';
 import '../../../src/screens/screens_navigation/screen_profile/screen_profile.dart';
 import '../../../src/screens/screens_navigation/screens_medical_prescription/screens/screen_include_prescription.dart';
+import '../../../src/screens/screens_navigation/screens_queries/logics/logic_type_user.dart';
 import '../../../src/screens/screens_navigation/screens_queries/screens/list_queries/screen_list_queries.dart';
 
 class NavbarProvider with ChangeNotifier {
   List<NavbarDTO> items = [
-    NavbarDTO(label: 'Home', widget: HomePage() , iconData: Icons.home),
-    NavbarDTO(label: 'Medicação',  widget: PrescricaoMedicamento(), iconData: Icons.medication_rounded),
-    NavbarDTO(label: 'Consulta',  widget: null, iconData: Icons.queue_play_next_rounded),
-    NavbarDTO(label: 'Perfil',  widget: const ScreenProfile(), iconData: Icons.person),
+    NavbarDTO(label: 'Home', widget: const HomePage(), iconData: Icons.home),
+    NavbarDTO(
+        label: 'Medicação',
+        widget: const PrescricaoMedicamento(),
+        iconData: Icons.medication_rounded),
+    // NavbarDTO(
+    //     label: 'Consulta',
+    //     widget: TypeUser.typeUserNavigator(context),
+    //     iconData: Icons.queue_play_next_rounded),
+    NavbarDTO(
+        label: 'Perfil', widget: const ScreenProfile(), iconData: Icons.person),
   ];
 
   int _indexSelecionado = 0;
